7b3820070a03b9ec308812141e979b21703d268b
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-07-03 12:02:27 -0300

    criar site

---
+++ java.js
@@ -0,0 +1,68 @@
+let button1 = document.querySelector('#xs');
+button1.addEventListener('click', function(){
+        if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = '';
+            this.innerText = '';
+            this.value = 'Ocultar';
+        } else {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none';
+            this.value = 'Marshal';
+        }
+}) 
+let button2 = document.querySelector('#xa');
+button2.addEventListener('click', function(){
+        if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = '';
+            this.innerText = '';
+            this.value = 'Ocultar';
+        } else {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none';
+            this.value = 'Lily';
+        }
+}) 
+let button3 = document.querySelector('#xb');
+button3.addEventListener('click', function(){
+        if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = '';
+            this.innerText = '';
+            this.value = 'Ocultar';
+        } else {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none';
+            this.value = 'Barney';
+        }
+}) 
+let button4 = document.querySelector('#xc');
+button4.addEventListener('click', function(){
+        if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = '';
+            this.innerText = '';
+            this.value = 'Ocultar';
+        } else {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none';
+            this.value = 'Ted';
+        }
+}) 
+let button5 = document.querySelector('#xd');
+button5.addEventListener('click', function(){
+        if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = '';
+            this.innerText = '';
+            this.value = 'Ocultar';
+        } else {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none';
+            this.value = 'Robin';
+        }
+}) 
+let button6 = document.querySelector('#xe');
+button6.addEventListener('click', function(){
+        if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = '';
+            this.innerText = '';
+            this.value = 'Ocultar';
+        } else {
+            this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none';
+            this.value = 'Oque é HIMYM?';
+        }
+}) 
+
+
